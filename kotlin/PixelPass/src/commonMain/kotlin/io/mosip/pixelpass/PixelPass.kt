4629f3a16aff4c099af050909fd79d2950251b4b
srikanth716 <srikanthsri7447@gmail.com>
2026-01-05 17:29:48 +0530

    [INJICERT] feat: add overloaded methods for enhanced key-value mapping with CLAIM-169 support
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- kotlin/PixelPass/src/commonMain/kotlin/io/mosip/pixelpass/PixelPass.kt
+++ kotlin/PixelPass/src/commonMain/kotlin/io/mosip/pixelpass/PixelPass.kt
@@ -8,144 +8,206 @@ import io.mosip.pixelpass.cbor.Utils
 import io.mosip.pixelpass.common.decodeFromBase64UrlFormat
 import io.mosip.pixelpass.exception.UnknownBinaryFileTypeException
 import io.mosip.pixelpass.shared.CLAIM_169_KEY_MAPPER
+import io.mosip.pixelpass.shared.CLAIM_169_REVERSE_KEY_MAPPER
 import io.mosip.pixelpass.shared.CLAIM_169_VALUE_MAPPER
 import io.mosip.pixelpass.shared.DEFAULT_ZIP_FILE_NAME
-import io.mosip.pixelpass.shared.CLAIM_169_REVERSE_KEY_MAPPER
 import io.mosip.pixelpass.shared.ZIP_HEADER
 import io.mosip.pixelpass.shared.decodeHex
 import io.mosip.pixelpass.types.ECC
 import io.mosip.pixelpass.utils.toMapWithKeyAndValueMapper
 import io.mosip.pixelpass.zlib.ZLib
-import nl.minvws.encoding.Base45
-import org.json.JSONArray
-import org.json.JSONObject
-import org.zeroturnaround.zip.ZipUtil
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.util.logging.Logger
+import nl.minvws.encoding.Base45
+import org.json.JSONArray
+import org.json.JSONObject
+import org.zeroturnaround.zip.ZipUtil
 
 class PixelPass {
-    private val logger = Logger.getLogger(PixelPass::class.java.name)
-    fun toJson(base64UrlEncodedCborEncodedString: String): Any {
-        val decodedData: ByteArray =
-            decodeFromBase64UrlFormat(base64UrlEncodedCborEncodedString)
-        val cbor: DataItem? =
-            CborDecoder(ByteArrayInputStream(decodedData)).decode()[0]
-        return Utils().toJson(cbor!!)
+  private val logger = Logger.getLogger(PixelPass::class.java.name)
+
+  fun toJson(base64UrlEncodedCborEncodedString: String): Any {
+    val decodedData: ByteArray = decodeFromBase64UrlFormat(base64UrlEncodedCborEncodedString)
+    val cbor: DataItem? = CborDecoder(ByteArrayInputStream(decodedData)).decode()[0]
+    return Utils().toJson(cbor!!)
+  }
+
+  fun generateQRCode(data: String, ecc: ECC = ECC.L, header: String = ""): String {
+    val dataWithHeader = generateQRData(data, header)
+    val qrcodeImage = convertQRDataIntoBase64(dataWithHeader, ecc)
+    return qrcodeImage
+  }
+
+  fun decode(data: String): String {
+    val decodedBase45Data = Base45.getDecoder().decode(data)
+    val decompressedData = ZLib().decode(decodedBase45Data)
+    return try {
+      val cborDecodedData = CborDecoder(ByteArrayInputStream(decompressedData)).decode()[0]
+
+      val json = Utils().toJson(cborDecodedData)
+      if (json.toString().startsWith('[') && json.toString().endsWith(']'))
+        (json as JSONArray).toString().replace("\\", "")
+      else (json as JSONObject).toString().replace("\\", "")
+    } catch (_: Exception) {
+      String(decompressedData)
+    }
+  }
+
+  fun decodeBinary(data: ByteArray): String {
+    if (String(data).startsWith(ZIP_HEADER)) {
+      var tempFile: File? = null
+      try {
+        tempFile = File.createTempFile("temp", ".zip")
+        tempFile.writeBytes(data)
+        if (ZipUtil.containsEntry(tempFile, DEFAULT_ZIP_FILE_NAME))
+          return String(ZipUtil.unpackEntry(tempFile, DEFAULT_ZIP_FILE_NAME))
+      } catch (e: Exception) {
+        throw e
+      } finally {
+        tempFile?.delete()
+      }
+    }
+    throw UnknownBinaryFileTypeException()
+  }
+
+  fun generateQRData(data: String, header: String = ""): String {
+    val parsedData: Any?
+    var compressedData = byteArrayOf()
+    var b45EncodedData = ""
+    try {
+      if (data.startsWith('[') && data.endsWith(']')) {
+        parsedData = JSONArray(data)
+      } else {
+        parsedData = JSONObject(data)
+      }
+      val toDataItem = Utils().toDataItem(parsedData)
+
+      val cborByteArrayOutputStream = ByteArrayOutputStream()
+      CborEncoder(cborByteArrayOutputStream).nonCanonical().encode(toDataItem)
+      compressedData = ZLib().encode(cborByteArrayOutputStream.toByteArray())
+    } catch (e: Exception) {
+      logger.severe(e.toString())
+      compressedData = ZLib().encode(data.toByteArray())
+    } finally {
+      b45EncodedData = String(Base45.getEncoder().encode(compressedData))
     }
 
-    fun generateQRCode(data: String, ecc: ECC = ECC.L, header: String = ""): String {
-        val dataWithHeader = generateQRData(data, header)
-        val qrcodeImage = convertQRDataIntoBase64(dataWithHeader, ecc)
-        return qrcodeImage
+    return (header + b45EncodedData)
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  fun getMappedData(
+    jsonData: JSONObject,
+    mapper: Map<String, String>,
+    cborEnable: Boolean = false,
+  ): String {
+    val mappedJson = JSONObject()
+    val iterator = jsonData.keys().iterator()
+    while (iterator.hasNext()) {
+      val next = iterator.next()
+      val key = mapper[next] ?: next
+      val value = jsonData.get(next.toString())
+      mappedJson.put(key.toString(), value)
     }
 
-    fun decode(data: String): String {
-        val decodedBase45Data = Base45.getDecoder().decode(data)
-        val decompressedData = ZLib().decode(decodedBase45Data)
-        return try {
-            val cborDecodedData = CborDecoder(ByteArrayInputStream(decompressedData)).decode()[0]
+    val payload = Utils().toDataItem(mappedJson)
 
-            val json = Utils().toJson(cborDecodedData)
-            if (json.toString().startsWith('[') && json.toString().endsWith(']'))
-                ( json as JSONArray ).toString().replace("\\","")
-            else
-                ( json as JSONObject ).toString().replace("\\","")
-        }catch (_: Exception){
-            String(decompressedData)
-        }
+    if (cborEnable) {
+      val cborByteArrayOutputStream = ByteArrayOutputStream()
+      CborEncoder(cborByteArrayOutputStream).encode(payload)
+      return cborByteArrayOutputStream.toByteArray().toHexString()
+    }
+    return payload.toString()
+  }
+
+  @OptIn(ExperimentalStdlibApi::class)
+  fun getMappedData(
+    jsonData: JSONObject,
+    keyMapper: Map<String, Int> = CLAIM_169_KEY_MAPPER,
+    valueMapper: Map<String, Map<Any, Int>> = CLAIM_169_VALUE_MAPPER,
+    cborEnable: Boolean = false,
+  ): Any {
+    val mappedJson = jsonData.toMapWithKeyAndValueMapper(keyMapper, valueMapper)
+
+    if (cborEnable) {
+      val payload = Utils().toDataItem(mappedJson)
+      val cborByteArrayOutputStream = ByteArrayOutputStream()
+      CborEncoder(cborByteArrayOutputStream).encode(payload)
+      return cborByteArrayOutputStream.toByteArray().toHexString()
+    }
+    return mappedJson
+  }
+
+  fun getMappedData(
+    jsonData: JSONArray,
+    keyMapper: Map<String, Int> = CLAIM_169_KEY_MAPPER,
+    valueMapper: Map<String, Map<Any, Int>> = CLAIM_169_VALUE_MAPPER,
+    cborEnable: Boolean = false,
+  ): JSONArray {
+    val mappedJsonArray = JSONArray()
+    (0 until jsonData.length()).forEach { i ->
+      when (val item = jsonData.get(i)) {
+        is JSONObject ->
+          mappedJsonArray.put(getMappedData(item, keyMapper, valueMapper, cborEnable))
+        else ->
+          throw IllegalArgumentException(
+            "Invalid input: Expected JSONObject at index $i, but found ${item::class.simpleName}"
+          )
+      }
+    }
+    return mappedJsonArray
+  }
+
+  fun decodeMappedData(data: String, mapper: Map<String, String>): String {
+    var jsonData: JSONObject
+    try {
+      val cborDecodedData = CborDecoder(ByteArrayInputStream(data.decodeHex())).decode()[0]
+      jsonData = (Utils().toJson(cborDecodedData) as JSONObject)
+    } catch (_: Exception) {
+      jsonData = JSONObject(data)
     }
 
-    fun decodeBinary(data: ByteArray): String {
-        if (String(data).startsWith(ZIP_HEADER)) {
-            var tempFile: File? = null
-            try {
-                tempFile = File.createTempFile("temp", ".zip")
-                tempFile.writeBytes(data)
-                if (ZipUtil.containsEntry(tempFile, DEFAULT_ZIP_FILE_NAME))
-                    return String(ZipUtil.unpackEntry(tempFile, DEFAULT_ZIP_FILE_NAME))
-            } catch (e: Exception) {
-                throw e
-            } finally {
-                tempFile?.delete()
-            }
-        }
-        throw UnknownBinaryFileTypeException()
+    val payload = JSONObject()
+    val iterator = jsonData.keys().iterator()
+    while (iterator.hasNext()) {
+      val next = iterator.next()
+      val key = mapper[next] ?: next
+      val value = jsonData.get(next.toString())
+      payload.put(key.toString(), value)
+    }
+    return payload.toString()
+  }
+
+  fun decodeMappedData(
+    data: String,
+    keyMapper: Array<Map<String, String>> = CLAIM_169_REVERSE_KEY_MAPPER,
+    valueMapperFunction: (JSONObject) -> JSONObject = Utils()::replaceValuesForClaim169,
+  ): String {
+    var jsonData: JSONObject
+    try {
+      val cborDecodedData = CborDecoder(ByteArrayInputStream(data.decodeHex())).decode()[0]
+      jsonData = (Utils().toJson(cborDecodedData) as JSONObject)
+    } catch (_: Exception) {
+      jsonData = JSONObject(data)
     }
 
-     fun generateQRData(
-        data: String,
-        header: String = ""
-    ): String {
-         val parsedData: Any?
-         var compressedData = byteArrayOf()
-         val b45EncodedData: String
-         try {
-             parsedData = if (data.startsWith('[') && data.endsWith(']')) {
-                 JSONArray(data)
-             } else {
-                 JSONObject(data)
-             }
-             val toDataItem = Utils().toDataItem(parsedData)
-
-             val cborByteArrayOutputStream = ByteArrayOutputStream()
-             CborEncoder(cborByteArrayOutputStream).nonCanonical().encode(toDataItem)
-             compressedData = ZLib().encode(cborByteArrayOutputStream.toByteArray())
-
-         }catch (e: Exception){
-             logger.severe(e.toString())
-             compressedData = ZLib().encode(data.toByteArray())
-         }finally {
-             b45EncodedData = String(Base45.getEncoder().encode(compressedData))
-         }
-
-        return (header + b45EncodedData)
+    keyMapper.forEachIndexed { index, mapper ->
+      jsonData = Utils().replaceKeysAtDepth(jsonData, mapper, index)
     }
+    return valueMapperFunction(jsonData).toString()
+  }
 
-    @OptIn(ExperimentalStdlibApi::class)
-    fun getMappedData(jsonData: JSONObject, keyMapper: Map<String,Int> = CLAIM_169_KEY_MAPPER, valueMapper: Map<String, Map<Any, Int>> = CLAIM_169_VALUE_MAPPER, cborEnable: Boolean = false): Any {
-        val mappedJson = jsonData.toMapWithKeyAndValueMapper(keyMapper,valueMapper)
-
-        if (cborEnable) {
-            val payload = Utils().toDataItem(mappedJson)
-            val cborByteArrayOutputStream = ByteArrayOutputStream()
-            CborEncoder(cborByteArrayOutputStream).encode(payload)
-            return cborByteArrayOutputStream.toByteArray().toHexString()
-        }
-        return mappedJson
+  fun decodeMappedData(
+    data: Array<String>,
+    keyMapper: Array<Map<String, String>> = CLAIM_169_REVERSE_KEY_MAPPER,
+    valueMapperFunction: (JSONObject) -> JSONObject = Utils()::replaceValuesForClaim169,
+  ): Array<String> {
+    val decodedJsonArray = mutableListOf<String>()
+    data.forEach { item ->
+      decodedJsonArray.add(decodeMappedData(item, keyMapper, valueMapperFunction))
     }
-
-    fun getMappedData(jsonData: JSONArray, keyMapper: Map<String,Int> = CLAIM_169_KEY_MAPPER, valueMapper: Map<String, Map<Any, Int>> = CLAIM_169_VALUE_MAPPER, cborEnable: Boolean = false): JSONArray {
-        val mappedJsonArray = JSONArray()
-        (0 until jsonData.length()).forEach { i ->
-            when (val item = jsonData.get(i)) {
-                is JSONObject -> mappedJsonArray.put(getMappedData(item,keyMapper,valueMapper,cborEnable))
-            }
-        }
-        return mappedJsonArray
-    }
-
-    fun decodeMappedData(data: String, keyMapper: Array<Map<String, String>> = CLAIM_169_REVERSE_KEY_MAPPER, valueMapperFunction: (JSONObject) -> JSONObject = Utils()::replaceValuesForClaim169): String {
-        var jsonData: JSONObject
-        try {
-            val cborDecodedData = CborDecoder(ByteArrayInputStream(data.decodeHex())).decode()[0]
-            jsonData =  (Utils().toJson(cborDecodedData) as JSONObject)
-        }catch (_: Exception){
-            jsonData = JSONObject(data)
-        }
-
-        keyMapper.forEachIndexed { index, mapper ->
-            jsonData = Utils().replaceKeysAtDepth(jsonData,mapper,index)
-        }
-        return valueMapperFunction(jsonData).toString()
-    }
-
-    fun decodeMappedData(data: Array<String>, keyMapper: Array<Map<String, String>> = CLAIM_169_REVERSE_KEY_MAPPER, valueMapperFunction: (JSONObject) -> JSONObject = Utils()::replaceValuesForClaim169): Array<String> {
-        val decodedJsonArray = mutableListOf<String>()
-        data.indices.forEach { i ->
-                decodedJsonArray.add(i,decodeMappedData(data[i],keyMapper,valueMapperFunction))
-        }
-        return decodedJsonArray.toTypedArray()
-    }
+    return decodedJsonArray.toTypedArray()
+  }
 }
